808cfcdcd6154e097adf1b7e55c7bf78dda8115c
unknown <lauromilagres@gmail.com>
2020-07-09 18:55:43 -0300

    http post

--- meat-app-starter/src/app/order/order.service.ts
+++ meat-app-starter/src/app/order/order.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@angular/core";
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
+import { Order,OrderItem } from "./order.model";
+import { Observable } from "rxjs/Observable";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import 'rxjs/add/operator/map';
+import { MEAT_API } from "app/app.api";
 
 @Injectable()
 export class OrderService {
 
-    constructor(private cartService: ShoppingCartService){}
+    constructor(private cartService: ShoppingCartService,
+                private http: Http){}
 
     cartItems():CartItem[]{
         return this.cartService.items;
@@ -23,7 +29,22 @@ export class OrderService {
         this.cartService.removeItem(item);
     }
 
+    clear(){
+        this.cartService.clear();
+    }
+
     itemsValue():number{
         return this.cartService.total();
+    }
+
+    checkOrder(order: Order):Observable<string>{
+        const headers = new Headers();
+        headers.append('Content-Type','application/json')
+        
+        return this.http.post(`${MEAT_API}/orders`,
+                                JSON.stringify(order),
+                                new RequestOptions({headers:headers})
+                            )
+            .map(response=> response.json());
     }
 }
